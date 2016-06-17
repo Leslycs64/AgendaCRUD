456f8c269485006e7162bef99c659d445c25f291
Juan Luis <luiscampos@uaz.edu.mx>
2016-06-16 20:22:57 -0500

    Se agregó la función para exportar a PDFs

--- contacto/forms.py
+++ contacto/forms.py
@@ -1,19 +1,15 @@
 # encoding:utf-8
 from django import forms
-
+apaterno = '"apaterno"'
 class FormContacto ( forms.Form ) :
-  nombre = forms.CharField ( widget=forms.TextInput (attrs={'class':'nombre'} ) , label='Nombre' , required=True )
-  apaterno = forms.CharField ( widget=forms.TextInput (attrs={'class':'apaterno'}) , label='Apellido Parterno')
-  amaterno = forms.CharField ( widget=forms.TextInput(attrs={'class':'amaterno'}) , label='Apellido Materno')
-  correo = forms.CharField ( widget=forms.TextInput (attrs={'class':'correo'}) , label='Correo Electrónico', required=True )
-  telefono = forms.CharField ( widget=forms.TextInput (attrs={'class':'telefono'}) , label='Teléfono' )
+  nombre = forms.CharField ( widget=forms.TextInput (attrs={
+    'class':'nombre',
+    'onblur':'getElementByName('+apaterno+').value=21',
+  }) , label='Nombre' , required=True )
+  apaterno = forms.CharField ( widget=forms.TextInput (attrs={'class':'apaterno',
+                                                              'onblur':'alert(1);'}) , label='Apellido Parterno')
+  amaterno = forms.CharField ( widget=forms.TextInput(attrs={'class':'amaterno','disabled': 'disabled'}) , label='Apellido Materno')
+  correo = forms.CharField ( widget=forms.TextInput (attrs={'class':'correo','disabled': 'disabled'}) , label='Correo Electrónico', required=True )
+  telefono = forms.CharField ( widget=forms.TextInput (attrs={'class':'telefono','disabled': 'disabled'}) , label='Teléfono' )
 
-  # class Meta:
-  #   model = Contacto
-  #   fields = ('nombre','apaterno','amaterno','correo','telefono')
-
-  # def save(self,commit=True):
-  #   contacto = super(Contacto,self).save(commit=False)
-  #
-  #   contacto.save()
-  #   return contacto
+#'disabled': 'disabled',
--- static/js/funciones.js
+++ static/js/funciones.js
@@ -3,3 +3,7 @@ $(document).ready(function() {
     $('.content').toggleClass('isOpen');
   });
 });
+
+function Hola(){
+  alert('Hola Mundo!');
+}
