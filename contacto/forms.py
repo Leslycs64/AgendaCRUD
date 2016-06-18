9ccab8ded0303989cc1803c98a80d1926c97faba
Juan Luis <luiscampos@uaz.edu.mx>
2016-06-18 00:15:26 -0500

    En en el formulario de contacto, se agregó la capacidad para habilidad los campos a llenar hasta que el previo contenga datos

--- contacto/forms.py
+++ contacto/forms.py
@@ -1,15 +1,31 @@
 # encoding:utf-8
 from django import forms
-apaterno = '"apaterno"'
+
 class FormContacto ( forms.Form ) :
   nombre = forms.CharField ( widget=forms.TextInput (attrs={
     'class':'nombre',
-    'onblur':'getElementByName('+apaterno+').value=21',
+    'onblur':'enablePaterno();',
   }) , label='Nombre' , required=True )
-  apaterno = forms.CharField ( widget=forms.TextInput (attrs={'class':'apaterno',
-                                                              'onblur':'alert(1);'}) , label='Apellido Parterno')
-  amaterno = forms.CharField ( widget=forms.TextInput(attrs={'class':'amaterno','disabled': 'disabled'}) , label='Apellido Materno')
-  correo = forms.CharField ( widget=forms.TextInput (attrs={'class':'correo','disabled': 'disabled'}) , label='Correo Electrónico', required=True )
-  telefono = forms.CharField ( widget=forms.TextInput (attrs={'class':'telefono','disabled': 'disabled'}) , label='Teléfono' )
 
-#'disabled': 'disabled',
+  apaterno = forms.CharField ( widget=forms.TextInput (attrs={
+    'class':'apaterno',
+    'disabled': 'disabled',
+    'onblur':'enableMaterno();'
+  }) , label='Apellido Parterno')
+
+  amaterno = forms.CharField ( widget=forms.TextInput(attrs={
+    'class':'amaterno',
+    'disabled': 'disabled',
+    'onblur' : 'enableCorreo();'
+  }) , label='Apellido Materno')
+
+  correo = forms.CharField ( widget=forms.TextInput (attrs={
+    'class':'correo',
+    'disabled': 'disabled',
+    'onblur' : 'enableTelefono();'
+  }) , label='Correo Electrónico', required=True )
+
+  telefono = forms.CharField ( widget=forms.TextInput (attrs={
+    'class':'telefono',
+    'disabled': 'disabled'
+  }) , label='Teléfono' )
