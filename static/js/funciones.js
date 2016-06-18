770242cb9a4675832015dc40c0422455d48e0f35
JuanLuisCB <luis4tazac@gmail.com>
2016-06-18 00:16:37 -0500

    Add files via upload

--- static/js/funciones.js
+++ static/js/funciones.js
@@ -3,3 +3,46 @@ $(document).ready(function() {
     $('.content').toggleClass('isOpen');
   });
 });
+
+function isEmptyField(campo){
+	var field = document.getElementById(campo).value;
+	if(field.length == 0){
+		return false;
+	}else{
+		return true;
+	}
+}
+
+function enablePaterno(){                     
+    var campo = document.getElementById("id_apaterno");
+    if (isEmptyField("id_nombre")){
+    	campo.disabled = false;	
+    	campo.focus();
+    }
+    
+}
+
+function enableMaterno(){
+    var campo = document.getElementById("id_amaterno");
+    if (isEmptyField("id_apaterno")){
+    	campo.disabled = false;	
+    	campo.focus();
+    }
+}
+
+function enableCorreo(){
+	var campo = document.getElementById("id_correo");
+    if (isEmptyField("id_amaterno")){
+    	campo.disabled = false;	
+    	campo.focus();
+    }	
+}
+
+function enableTelefono(){
+	var campo = document.getElementById("id_telefono");
+    if (isEmptyField("id_correo")){
+    	campo.disabled = false;	
+    	campo.focus();
+    }	
+}
+/*.removeAttr("disabled");*/
